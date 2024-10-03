f40b4ceafbec69240cb18553518f750110ea3c23
PraveenGarlapati7 <praveengideongarlapati@gmail.com>
2024-10-03 15:46:19 +0530

    feat(js): bubblesort

---
+++ algorithms/bubblesort.js
@@ -0,0 +1,22 @@
+function swap(arr,i,j){
+    [arr[i],arr[j]] = [arr[j],arr[i]];
+}
+
+function bubbleSort(arr){
+    var noSwaps;
+    for(let i = arr.length ; i > 0; i--){
+        noSwaps = true;
+        for(let j = 0; j < i -1; j++){
+            if(arr[j]>arr[j+1]){
+                swap(arr,j,j+1);
+                noSwaps = false
+            }
+        }
+        // console.log(arr)
+        if(noSwaps) break;
+        // console.log("One Pass completed")
+    }
+    return arr;
+}
+
+console.log(bubbleSort([7,3,8,2,9,5,1,4,6]));
